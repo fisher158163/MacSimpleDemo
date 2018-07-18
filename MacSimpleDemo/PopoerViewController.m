7dbc6ccc1d119715329ecaa9d18ff62b783e42be
liyu <13798464518@163.com>
2018-07-18 16:37:59 +0800

    新增内容

--- MacSimpleDemo/PopoerViewController.m
+++ MacSimpleDemo/PopoerViewController.m
@@ -7,12 +7,20 @@
 //
 
 #import "PopoerViewController.h"
+// 选择照片、相机拍照
 #import <Quartz/Quartz.h>
+// 事件库框架
+#import <EventKit/EventKit.h>
 
 @interface PopoerViewController ()<NSUserNotificationCenterDelegate>
 @property (weak) IBOutlet NSImageView *imageView;
 @property (weak) IBOutlet NSImageView *pictureImageView;
 @property (weak) IBOutlet NSTextField *label;
+@property (weak) IBOutlet NSTextField *pasteLabel;
+@property (weak) IBOutlet NSView *pickerBgView;
+@property (weak) IBOutlet NSDatePicker *datePicker1;
+@property (weak) IBOutlet NSDatePicker *datePicker2;
+@property (weak) IBOutlet NSTextField *dateLabel;
 @property (nonatomic,strong) NSFont *font;
 @end
 
@@ -20,8 +28,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.layer.backgroundColor = [NSColor orangeColor].CGColor;
+    self.view.wantsLayer = YES;
+    self.view.layer.backgroundColor = [NSColor whiteColor].CGColor;
+    self.pickerBgView.wantsLayer = YES;
+    self.pickerBgView.layer.backgroundColor = [NSColor cyanColor].CGColor;
     [self setupImageView];
+    [self setupDatePicker];
+}
+
+- (void)setupDatePicker {
+     // 初始化选中当前日期
+    [self.datePicker1 setDateValue:[NSDate date]];
+    [self.datePicker1 setAction:@selector(updateDateResult:)];
+    [self.datePicker2 setDateValue:[NSDate date]];
+    [self.datePicker2 setAction:@selector(updateDateResult:)];
 }
 
 - (void)setupImageView {
@@ -177,6 +197,178 @@
 - (void)openUrlBySafariWithUrlString:(NSString *)urlString {
     NSURL *url = [NSURL URLWithString:urlString];
     [[NSWorkspace sharedWorkspace] openURL: url];
+}
+
+// (日历、提醒事项)添加框架EventKit/EventKit.h>、在info.plist中添加授权描述
+- (IBAction)addReminder:(NSButton *)sender {
+    [self addReminderItem];
+//  [self getReminders];
+}
+
+// 添加提醒事项
+- (void)addReminderItem {
+    EKEventStore *eventStore = [[EKEventStore alloc]init];
+    [eventStore requestAccessToEntityType:EKEntityTypeReminder completion:^(BOOL granted, NSError * _Nullable error) {
+        EKReminder *reminder = [EKReminder reminderWithEventStore:eventStore];
+        reminder.calendar = [eventStore defaultCalendarForNewReminders];
+        reminder.title = @"提醒我15分钟后喝水！";
+        reminder.calendar = [eventStore defaultCalendarForNewReminders];;
+        EKAlarm *alarm = [EKAlarm alarmWithAbsoluteDate:[NSDate dateWithTimeIntervalSinceNow:20]];
+        [reminder addAlarm:alarm];
+        NSError *err = nil;
+        [eventStore saveReminder:reminder commit:YES error:&err];
+        if (error) {
+            NSLog(@"error = %@",error);
+        } else {
+            NSLog(@"保存到提醒事项成功");
+        }
+    }];
+}
+
+- (void)getReminders {
+    EKEventStore *eventStore = [[EKEventStore alloc] init];
+    NSArray *reminderArray = [eventStore calendarsForEntityType:EKEntityTypeReminder];
+    [eventStore requestAccessToEntityType:EKEntityTypeReminder completion:^(BOOL granted, NSError * _Nullable error) {
+        if (!granted) {
+            return ;
+        }
+        //获取提醒事项的分类
+        for (EKCalendar *calendar in reminderArray) {
+            NSLog(@"title：%@ ， type：%ld , calendarIdentifier：%@",calendar.title,(long)calendar.type,calendar.calendarIdentifier);
+        }
+        //来查找所有的reminders
+        NSPredicate *pre =[eventStore predicateForRemindersInCalendars:@[reminderArray.firstObject]];
+        //异步方法。
+        [eventStore fetchRemindersMatchingPredicate:pre completion:^(NSArray<EKReminder *> * _Nullable reminders) {
+            // 异步查找出提醒事项数组reminders，这里可根据需求进一步进行对数组的操作
+            /*
+             reminders 数组里存的是EKReminder 对象。
+             列举EKReminder 的一些属性：
+             title：标题
+             notes：备注
+             priority：优先级（NSUInteger）  0无级别，1级别高---9级别低（1-4高，5中等，6-9低）
+             completed：是否已完成
+             completionDate：完成时间
+             alarms:提醒数组（数组里是EKAlarm对象， 可以获得跟闹钟相关的数据，如具体时间，偏移秒数...）
+             */
+            for (EKReminder *reminder in reminders) {
+                NSLog(@"reminder - title：%@ ， notes：%@",reminder.title,reminder.notes);
+            }
+        }];
+    }];
+}
+
+// 添加日历
+- (IBAction)addCalendar:(NSButton *)sender {
+    [self addCalendarItem];
+}
+
+- (void)addCalendarItem {
+    EKEventStore *eventStore = [[EKEventStore alloc]init];
+    EKEvent *event = [EKEvent eventWithEventStore:eventStore];
+    event.title = @"日历测试";
+    // event.location = location;
+    // [NSDate dateWithTimeIntervalSinceNow:10];
+    // [NSDate dateWithTimeIntervalSinceNow:20];
+    NSDate *startDate = [NSDate dateWithTimeIntervalSinceNow:10];
+    NSDate *endDate = [NSDate dateWithTimeIntervalSinceNow:20];
+    // 设定事件开始时间
+    event.startDate = startDate;
+    // 设定事件结束时间
+    event.endDate = endDate;
+    // 添加提醒 可以添加多个，设定事件多久以前开始提醒
+    // event.allDay = YES;
+    // 在事件前多少秒开始事件提醒 -5.0f
+    EKAlarm *alarm = [EKAlarm alarmWithRelativeOffset:-5.f];
+    [event addAlarm:alarm];
+    [event setCalendar:[eventStore defaultCalendarForNewEvents]];
+    NSError *error;
+    [eventStore saveEvent:event span:EKSpanThisEvent commit:YES error:&error];
+    NSLog(@"保存成功");
+}
+
+// 获取日历事件
+- (void)getCalendar {
+    EKEventStore *store = [[EKEventStore alloc] init];
+    [store requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError * _Nullable error) {
+        NSLog(@"granted：%d", granted);
+        if (granted) {
+            NSDate *startDate = [self dateFromString:@"2017-08"]; // 事件段，开始时间
+            NSDate *endDate = [self dateFromString:@"2018-01"]; // 结束时间，取中间
+            NSPredicate *fetchCalendarEvents = [store predicateForEventsWithStartDate:startDate endDate:endDate calendars:nil];
+            NSArray<EKEvent *> *eventList = [store eventsMatchingPredicate:fetchCalendarEvents];
+            NSArray<EKEvent *> *events = [eventList filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(EKEvent * _Nullable event, NSDictionary<NSString *,id> * _Nullable bindings) {
+                return event.calendar.subscribed;
+            }]];
+            NSLog(@"events:<<<<<<<<<<<< %@ >>>>>>>>>>>",events);
+            for (EKEvent *event in events) {
+                NSLog(@"title : %@ , startDate : %@ , endDate : %@ , calendarItemIdentifier : %@",event.title,event.startDate,event.endDate,event.calendarItemIdentifier);
+            }
+        }
+    }];
+}
+
+- (NSDate *)dateFromString:(NSString *)dateString {
+    //设置转换格式
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init] ;
+    [formatter setDateFormat:@"yyyy-MM"];
+    NSDate *date = [formatter dateFromString:dateString];
+    return date;
+}
+
+- (IBAction)copy:(NSButton *)sender {
+    [self pasteString];
+}
+
+// 复制内容到剪贴板
+- (void)pasteString {
+    NSPasteboard *paste = [NSPasteboard generalPasteboard];
+    [paste clearContents];
+    [paste writeObjects:@[@"123456789"]];
+}
+
+- (IBAction)paste:(NSButton *)sender {
+    [self getPasteString];
+}
+
+// 获得剪贴板的内容
+- (void)getPasteString {
+    NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+// 粘贴板类型
+/* NSPasteboardTypeString;
+    NSPasteboardTypePDF;
+    NSPasteboardTypeTIFF;
+    NSPasteboardTypePNG;
+    NSPasteboardTypeRTF;
+    NSPasteboardTypeRTFD;
+    NSPasteboardTypeHTML;
+    NSPasteboardTypeTabularText;
+    NSPasteboardTypeFont;
+    NSPasteboardTypeRuler;
+    NSPasteboardTypeColor;
+    NSPasteboardTypeSound;
+    NSPasteboardTypeMultipleTextSelection;
+    NSPasteboardTypeFindPanelSearchOptions;
+*/
+    if ([[pasteboard types] containsObject:NSPasteboardTypeString]) {
+        NSString *str = [pasteboard stringForType:NSPasteboardTypeString];
+        self.pasteLabel.stringValue = str;
+        NSLog(@"粘贴的文字：%@",str);
+    }
+}
+
+- (void)updateDateResult:(NSDatePicker *)datePicker {
+    // 拿到当前选择的日期
+    NSDate *theDate = [datePicker dateValue];
+    NSLog(@"日期：%@", theDate);
+    if (theDate) {
+        // 把选择的日期格式化成想要的形式
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+        NSString *dateString = [formatter stringFromDate:theDate];
+        self.dateLabel.stringValue = dateString;
+        NSLog(@"日期：%@", dateString);
+    }
 }
 
 - (void)dealloc {
